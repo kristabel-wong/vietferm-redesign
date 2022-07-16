8c80a4c019f2105a1e5dfa672698583bdda289bc
kristabel-wong <kristabel.wong@hotmail.com>
2022-07-17 09:49:03 +1000

    added swiper carousel and basic css

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="home">
-        <div>
-            Sạch từ nguyên liệu đầu vào
+        <div class="top">
+            <div>
+                Sạch từ nguyên liệu đầu vào
+            </div>
+            <div>
+                Sản xuất bằng tâm huyết và trí tuệ
+            </div>
+            <div>
+                Lựa chọn tuyệt vời cho sức khoẻ
+            </div>
         </div>
-        <div>
-            Sản xuất bằng tâm huyết và trí tuệ
-        </div>
-        <div>
-            Lựa chọn tuyệt vời cho sức khoẻ
+
+        <div class="carousel">
+            test
         </div>
     </div>
 </template>
@@ -20,5 +26,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.top {
+    float: left;
+}
+
+.carousel {
+    float: right;
+}
+
+.home {
+    width: 60vw;
+    margin: 0 auto;
+}
 
 </style>
