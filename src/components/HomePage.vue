a79ff85b122105966c06634aac7b9eab88c33e6c
kristabel-wong <kristabel.wong@hotmail.com>
2022-07-17 13:36:38 +1000

    added next section

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,25 +1,45 @@
 <template>
     <div class="home">
-        <div class="top">
-            <div>
-                Sạch từ nguyên liệu đầu vào
+        <div class="flex">
+            <div class="top">
+                <div>
+                    Sạch từ nguyên liệu đầu vào
+                </div>
+                <div>
+                    Sản xuất bằng tâm huyết và trí tuệ
+                </div>
+                <div>
+                    Lựa chọn tuyệt vời cho sức khoẻ
+                </div>
             </div>
-            <div>
-                Sản xuất bằng tâm huyết và trí tuệ
-            </div>
-            <div>
-                Lựa chọn tuyệt vời cho sức khoẻ
+
+            <div class="carousel">
+                <swiper class="swiper" :options="swiperOption">
+                    <swiper-slide><img alt="image1" src="../assets/1.png"></swiper-slide>
+                    <swiper-slide><img alt="image2" src="../assets/2.png"></swiper-slide>
+                    <swiper-slide><img alt="image3" src="../assets/3.png"></swiper-slide>
+                    <div class="swiper-button-prev" slot="button-prev"></div>
+                    <div class="swiper-button-next" slot="button-next"></div>
+                </swiper>
             </div>
         </div>
-
-        <div class="carousel">
-            <swiper class="swiper" :options="swiperOption">
-                <swiper-slide><img alt="image1" src="../assets/1.png"></swiper-slide>
-                <swiper-slide><img alt="image2" src="../assets/2.png"></swiper-slide>
-                <swiper-slide><img alt="image3" src="../assets/3.png"></swiper-slide>
-                <div class="swiper-button-prev" slot="button-prev"></div>
-                <div class="swiper-button-next" slot="button-next"></div>
-            </swiper>
+        <div>
+            <div class="images">
+                <div class="text">
+                    <div>
+                        <span>Sức khoẻ </span> của người dùng là ưu tiên hàng đầu.
+                    </div>
+                    <div>
+                        Mỗi sản phẩm đều là <span> tâm huyết </span> và sự tự hào.
+                    </div>
+                    <div>
+                        Lưu giữ <span> hương vị truyền thống </span> trong suốt 30 năm.
+                    </div>
+                </div>
+                <img alt="image4" src="../assets/4.png">
+                <img alt="image5" src="../assets/5.png">
+                <img alt="image6" src="../assets/6.png">
+            </div>
         </div>
     </div>
 </template>
@@ -55,11 +75,22 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+.home {
+    width: 60vw;
+    margin: 0 auto;
+    border: 1px solid red;
+}
+
+.flex {
+    display: flex;
+    justify-content: space-between;
+}
+
 .top {
-    float: left;
-    position: absolute;
-    top: 40%;
+    margin-top: 10%;
     font-size: 1.3em;
+    /* border: 1px solid red; */
+    width: 50%;
 }
 
 .top div {
@@ -68,15 +99,34 @@ export default {
 
 .carousel {
     margin: 0 30px;
-    max-width: 60%;
+    max-width: 50%;
     /* border: 1px solid red; */
-    float: right;
 }
 
-.home {
-    width: 60vw;
-    margin: 0 auto;
+.carousel img {
+    width: 80%;
+}
+
+.images {
+    margin: 40px 0;
+}
+
+.images img {
+    margin: 0 10px;
+}
+
+.text {
+    /* position: relative; */
+    font-size: 1.3em;
+    text-align: right;
     border: 1px solid red;
+    /* width: 50%; */
+    /* right: 10px; */
+    color: #7C848C;
+}
+
+span {
+    color: #F0BB4A
 }
 
 .swiper-button-next, .swiper-button-prev {
